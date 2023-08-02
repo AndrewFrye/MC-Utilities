f5176d245bc6e3f7a5dea2e41462118d95b9a3de
Andrew Frye <dewdew72805@gmail.com>
2023-08-01 20:48:19 -0400

    Started server dashboard rebuild

---
+++ Applications/MinecraftServerDashboard/Server/Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using ButtonData.Json;
+using System.Net.Sockets;
+using System.Net;
+using System;
+
+namespace Server;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
+
+        server.Start();
+        Console.WriteLine("Server has started on 127.0.0.1:80.{0}Waiting for a connection…", Environment.NewLine);
+
+        TcpClient client = server.AcceptTcpClient();
+
+        Console.WriteLine("A client connected.");
+    }
+}
